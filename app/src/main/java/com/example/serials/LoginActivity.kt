e4943a212bc9aa4876d035550deacadf6ea3f051
BlackCaatt <BlackCaatt@mail.ru>
2019-05-19 16:58:56 +0300

    log in

--- app/src/main/java/com/example/serials/LoginActivity.kt
+++ app/src/main/java/com/example/serials/LoginActivity.kt
@@ -1,15 +1,20 @@
 package com.example.serials
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.util.Log
+import android.view.View
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_login.*
 
-class LoginActivity : AppCompatActivity(), TextWatcher {
+class LoginActivity : AppCompatActivity(), TextWatcher, View.OnClickListener {
 
     private val TAG = "LoginActivity"
+    private lateinit var mAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,12 +24,34 @@ class LoginActivity : AppCompatActivity(), TextWatcher {
         login_btn.isEnabled = false
         email_input.addTextChangedListener(this)
         password_input.addTextChangedListener(this)
+        login_btn.setOnClickListener(this)
+
+        mAuth = FirebaseAuth.getInstance()
     }
+
+    override fun onClick(view: View) {
+        val email = email_input.text.toString()
+        val password = password_input.text.toString()
+        if (validate(email, password)) {
+            mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener {
+                if (it.isSuccessful) {
+                    startActivity(Intent(this, MainActivity::class.java))
+                    finish()
+                }
+            }
+        } else {
+            Toast.makeText(this, "please enter email and password", Toast.LENGTH_SHORT)
+                .show()
+        }
+    }
+
     override fun afterTextChanged(s: Editable?) {
-        login_btn.isEnabled =
-        email_input.text.toString().isNotEmpty() &&
-                password_input.text.toString().isNotEmpty()
+        login_btn.isEnabled = validate(email_input.text.toString(), password_input.text.toString())
     }
+
+    private fun validate(email: String, password: String) =
+        email.isNotEmpty() &&
+                password.isNotEmpty()
 
     override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
     }
--- app/src/main/java/com/example/serials/MainActivity.kt
+++ app/src/main/java/com/example/serials/MainActivity.kt
@@ -11,7 +11,7 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity(0) {
     private val TAG = "MainActivity"
-    private lateinit var nAuth:FirebaseAuth
+    private lateinit var mAuth:FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,14 +19,22 @@ class MainActivity : BaseActivity(0) {
         setupBottomNavigation()
         Log.d(TAG, "onCreate")
 
-        nAuth = FirebaseAuth.getInstance()
-        nAuth.signOut()
+        mAuth = FirebaseAuth.getInstance()
 
+        sign_out_text.setOnClickListener{
+            mAuth.signOut()
+        }
+        mAuth.addAuthStateListener {
+            if (it.currentUser == null) {
+                startActivity(Intent(this, LoginActivity::class.java))
+                finish()
+            }
+        }
     }
 
     override fun onStart() {
         super.onStart()
-        if (nAuth.currentUser == null){
+        if (mAuth.currentUser == null){
             startActivity(Intent(this, LoginActivity::class.java))
             finish()
         }
