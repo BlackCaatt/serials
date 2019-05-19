9b7dd847261abb6c8bf6dff087c48c74d12c8299
BlackCaatt <BlackCaatt@mail.ru>
2019-05-19 14:10:18 +0300

    empty/not

--- app/src/main/java/com/example/serials/LoginActivity.kt
+++ app/src/main/java/com/example/serials/LoginActivity.kt
@@ -2,14 +2,33 @@ package com.example.serials
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
+import kotlinx.android.synthetic.main.activity_login.*
 
-class LoginActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity(), TextWatcher {
+
     private val TAG = "LoginActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
         Log.d(TAG, "onCreate")
+
+        login_btn.isEnabled = false
+        email_input.addTextChangedListener(this)
+        password_input.addTextChangedListener(this)
+    }
+    override fun afterTextChanged(s: Editable?) {
+        login_btn.isEnabled =
+        email_input.text.toString().isNotEmpty() &&
+                password_input.text.toString().isNotEmpty()
+    }
+
+    override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+    }
+    override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+
     }
 }
