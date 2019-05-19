76403b721d53935b73bc076a164f67667f950d9c
BlackCaatt <BlackCaatt@mail.ru>
2019-05-19 13:27:13 +0300

    firebase,login

--- app/src/main/java/com/example/serials/MainActivity.kt
+++ app/src/main/java/com/example/serials/MainActivity.kt
@@ -5,11 +5,13 @@ import android.nfc.Tag
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : BaseActivity(0) {
     private val TAG = "MainActivity"
+    private lateinit var nAuth:FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,6 +19,16 @@ class MainActivity : BaseActivity(0) {
         setupBottomNavigation()
         Log.d(TAG, "onCreate")
 
+        nAuth = FirebaseAuth.getInstance()
+        nAuth.signOut()
 
+    }
+
+    override fun onStart() {
+        super.onStart()
+        if (nAuth.currentUser == null){
+            startActivity(Intent(this, LoginActivity::class.java))
+            finish()
+        }
     }
 }
