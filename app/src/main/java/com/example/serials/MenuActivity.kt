6be52c61688d6c2a938e07c3a655e88c0fba2f3a
BlackCaatt <BlackCaatt@mail.ru>
2019-05-21 21:02:12 +0300

    norm

--- app/src/main/java/com/example/serials/MenuActivity.kt
+++ app/src/main/java/com/example/serials/MenuActivity.kt
@@ -2,22 +2,73 @@ package com.example.serials
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.GridLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.util.Log
+<<<<<<< HEAD
 import com.example.serials.FirebaseHelper
 import com.example.serials.ValueEventListenerAdapter
+=======
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.ImageView
+import com.bumptech.glide.Glide
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
+import com.squareup.picasso.Picasso
+import com.xwray.groupie.GroupAdapter
+import com.xwray.groupie.Item
+import com.xwray.groupie.ViewHolder
+>>>>>>> parent of c0ce342... Revert "idk"
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_menu.*
+import kotlinx.android.synthetic.main.serial_row.view.*
+
 
 
 class MenuActivity : BaseActivity(1) {
+<<<<<<< HEAD
     private val TAG = "MenuActivity"
     private lateinit var mFirebase: FirebaseHelper
 
+=======
+>>>>>>> parent of c0ce342... Revert "idk"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
         setupBottomNavigation()
-        Log.d(TAG, "onCreate")
+
+        fetchSerials()
+
+    }
+
+    private fun fetchSerials() {
+        val ref = FirebaseDatabase.getInstance().getReference("/serials")
+        ref.addListenerForSingleValueEvent(object: ValueEventListener {
+
+            override fun onDataChange(p0: DataSnapshot){
+                val adapter = GroupAdapter<ViewHolder>()
+
+                p0.children.forEach{
+                    Log.d("newMessage", it.toString())
+                    val serial = it.getValue(Serial::class.java)
+                    if (serial != null) {
+                        adapter.add(SerialItem(serial))
+                    }
+                }
+
+                recyclerview_serials.adapter = adapter
+            }
+
+            override fun onCancelled(p0: DatabaseError) {
+
+            }
+        })
 
         mFirebase = FirebaseHelper(this)
 
@@ -29,5 +80,26 @@ class MenuActivity : BaseActivity(1) {
 
 
     }
+
 }
+class Serial(val name: String, val pic: String){
+    constructor() : this("","")
+}
+
+class SerialItem(val serial: Serial): Item<ViewHolder>() {
+    override fun bind(viewHolder: ViewHolder, position: Int) {
+        viewHolder.itemView.textview_serials.text = serial.name
+
+        Picasso.get().load(serial.pic).into(viewHolder.itemView.imageview_serials)
+    }
+
+    override fun getLayout(): Int {
+        return R.layout.serial_row
+    }
+}
+<<<<<<< HEAD
 data class Serials(val name:String = "", val image: String = "")
+=======
+
+
+>>>>>>> parent of c0ce342... Revert "idk"
--- app/src/main/java/com/example/serials/RegisterActivity.kt
+++ app/src/main/java/com/example/serials/RegisterActivity.kt
@@ -1,12 +1,43 @@
 package com.example.serials
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_register.*
 
 class RegisterActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        register_btn.setOnClickListener{
+            performRegister()
+        }
+    }
+
+    private fun performRegister() {
+        val email = email_input.text.toString()
+        val password = password_input.text.toString()
+
+        if (email.isEmpty() || password.isEmpty()) {
+            Toast.makeText(this, "Пожалуйста введите почту/пароль", Toast.LENGTH_SHORT).show()
+            return
+        }
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener{
+                if(!it.isSuccessful) return@addOnCompleteListener
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+                Log.d("Main", "Successfully created")
+            }
+            .addOnFailureListener{
+                Toast.makeText(this, "Ошибка в данных", Toast.LENGTH_SHORT).show()
+                Log.d("Main", "Failed to create user")
+            }
     }
 }
+
