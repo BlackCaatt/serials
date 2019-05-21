e81a120b3250a8eba304c8c0c4c6243a9243b42d
BlackCaatt <BlackCaatt@mail.ru>
2019-05-21 18:24:52 +0300

    anotherway?

--- app/src/main/java/com/example/serials/MenuActivity.kt
+++ app/src/main/java/com/example/serials/MenuActivity.kt
@@ -3,17 +3,31 @@ package com.example.serials
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import com.example.serials.FirebaseHelper
+import com.example.serials.ValueEventListenerAdapter
 import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MenuActivity : BaseActivity(1) {
     private val TAG = "MenuActivity"
+    private lateinit var mFirebase: FirebaseHelper
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_menu)
         setupBottomNavigation()
         Log.d(TAG, "onCreate")
 
+        mFirebase = FirebaseHelper(this)
+
+        mFirebase.database.child("serials")
+            .addValueEventListener(ValueEventListenerAdapter{
+                val posts = it.children.map { it.getValue(Serials::class.java)!! }
+                Log.d(TAG, "feedPosts: ${posts.joinToString("\n", "\n")}")
+            })
+
+
     }
 }
+data class Serials(val name:String = "", val image: String = "")
